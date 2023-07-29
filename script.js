544ef5b0cfbbd79421b3f4c3d32c35b5178b5ac1
Piumi-Anuradha <piumianuradha26@gmail.com>
2023-07-29 17:32:08 +0000

    update the files

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 var minutes;
 var seconds;
-
-function focus(){
-
-    minutes=25;
-    seconds=0;
+function work(){
+    
+    minutes = 25;
+    seconds = 0;
 
     var interval = setInterval(
         function(){
-            var dots = document.getElementById("timer");
+
             
-            if(seconds==0)
+            var dots = document.getElementById("timer");
+            if (seconds==0 && minutes==0){
+                clearInterval(interval)
+                dots.innerHTML = "00:00"
+                dots.style.backgroundColor = "green"
+
+            }
+
+            else if(seconds==0){
             minutes = minutes -1;
             seconds = 59;
             dots.innerHTML = minutes + ":" + seconds
             }else if(seconds>0){
             seconds = seconds -1;
             dots.innerHTML = minutes + ":" + seconds
+            }
 
-        }
+        },
+        1000
     )
 
     
